4ed2437cb6e76f39bd2953b7174d74128514e084
mcollie1 <marcus.collier7@yahoo.com>
2014-04-04 19:15:00 -0400

    Created Bookmark resource and updated index and show views

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,8 +1,10 @@
 class BookmarksController < ApplicationController
   def index
+    @bookmarks = Bookmark.all
   end
 
   def show
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,9 @@
 require 'faker'
 
 24.times do 
-  t = Topic.create(title: Faker::Lorem.word)
-  3.times do
-    t.bookmarks.create(url: Faker::Internet.url)
-  end
+  Bookmark.create(title: Faker::Lorem.word, url: Faker::Internet.url)
 end
 
 puts "Seed finished"
-puts "#{Topic.count} topics created"
 puts "#{Bookmark.count} bookmarks created"
+
