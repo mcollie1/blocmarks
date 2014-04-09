75ddc808f0a2d70d5a1709a4905c9df7f34e6891
mcollie1 <marcus.collier7@yahoo.com>
2014-04-08 23:02:45 -0400

    deleted code blot

--- app/models/email_processor.rb
+++
@@ -1,5 +0,0 @@
-class EmailProcessor
-  def self.process(email)
-    Bookmark.create!({ title: email.subject, url: email.body })
-  end
-end
--- app/models/incoming.rb
+++
@@ -1,3 +0,0 @@
-class Incoming < ActiveRecord::Base
-  
-end
