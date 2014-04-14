93a1df60f6bb405ba887b872096133863b3de5ee
mcollie1 <marcus.collier7@yahoo.com>
2014-04-13 23:52:56 -0400

    cleaned up users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @vote = @user.votes.find(params[:id])
-    @url = @vote.url
-    @bookmark = @url.bookmark
   end
 
   def create
