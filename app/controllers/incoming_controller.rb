5dbf9706527cc2591bad1dcef36b8953e764fbd4
mcollie1 <marcus.collier7@yahoo.com>
2014-05-12 00:10:27 -0400

    removed email variable from incoming_controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -9,19 +9,14 @@ class IncomingController < ApplicationController
     body_plain = params["body-plain"]
     user = User.find_by_email(sender)
     id = user.id
-    email = user.email
 
-    entry = Bookmark.create_with!(title: subject, url: body_plain).find_or_create_by!(user_id: id)
-    user_bookmarks = user.bookmarks
-    titles = user.bookmarks.map { |b| b["title"] }
-    urls = user.urls.map { |u| u["url"] }
+    entry = Bookmark.find_or_create_by!(user_id: id, title: subject)
+    urls = entry.urls.map { |u| u["url"] }
 
-    if titles.include?(subject) && urls.exclude?(body_plain)
+    if urls.exclude?(body_plain)
       user_bookmark = user_bookmarks.find_by_title(subject)
       user_bookmark.urls.create!(url: body_plain, user_id: id)
-    elsif titles.include?(subject) && urls.include?(body_plain)
-      nil
-    else
+    elsif !urls.include?(body_plain)
       entry.urls.create!(url: body_plain) 
     end
     
